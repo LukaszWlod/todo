370cefdeddaef870344aa235c9a8e50eb0072406
Łukasz W <lukasz1023@gmail.com>
2022-02-01 18:37:23 +0100

    first commit

---
+++ src/main/java/com/luwl/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.luwl.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luwl/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luwl.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
